6e5ed9285ff49e18d3afc0ffd34a98256086acbe
Wesley Fernandes <wesleyfer72@gmail.com>
2023-03-14 17:44:57 -0300

    Adicionando extração da primeira página, resta a automação para as restantes.

--- extract.py
+++ extract.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from bs4 import BeautifulSoup
-#import pandas as pd
-#import numpy as np
 import time
 
 driver = webdriver.Chrome()
